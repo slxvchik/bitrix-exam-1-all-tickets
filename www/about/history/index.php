879be63d1167c95771676f251e333ab1d88223ac
Vyacheslav Antonov <vyacheslav.antonov.job@gmail.com>
2025-09-30 11:09:58 +0300

    Setting page metadata

--- www/about/history/index.php
+++ www/about/history/index.php
@@ -1,4 +1,6 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("keywords", "Проминжиниринг, История");
+$APPLICATION->SetPageProperty("description", "История компании и путь к успеху");
 $APPLICATION->SetTitle("История");
 ?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
